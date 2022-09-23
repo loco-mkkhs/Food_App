157e9a979f6d3a6c52542df863c87cc78ccf96ae
loco-mkkhs <105632704+loco-mkkhs@users.noreply.github.com>
2022-09-23 15:44:39 +0300

    android studio projects

--- app/src/main/java/com/example/food_app/MainActivity.kt
+++ app/src/main/java/com/example/food_app/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.food_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        val bottomnav = findViewById<BottomNavigationItemView>(R.id.bottomnav)
+        val navController = findNavController(R.id.host_fragment)
+
+        bottomnav.setupWithNavController(navController)
+        
+        
     }
+
 }
+
+private fun BottomNavigationItemView.setupWithNavController(navController: NavController) {
+
+}
+
+
+
+
+
